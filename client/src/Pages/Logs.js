861c12bf05dc0d11358ba059afe72c8159719d5f
Dillon Reinwald <dreinwaldmail@gmail.com>
2018-11-11 21:13:36 -0800

    added heartrate button to logs

--- client/src/Pages/Logs.js
+++ client/src/Pages/Logs.js
@@ -5,37 +5,42 @@ class Logs extends Component {
     render() {
         return (
             <div className="card-deck">
-                <div className="card btn " data-toggle="modal"  data-target="#workoutModal" >
+                <div className="card btn " data-toggle="modal"  data-target="#workoutModal" value="Arms" >
                     <img className="card-img-top" src="./images/arms2.jpg" alt="Card image cap" />
                     <div className="card-body">
-                        <h5 className="card-title text-center">Arms</h5>
+                        <h5 className="card-title text-center" >Arms</h5>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card btn" data-toggle="modal"  data-target="#workoutModal" value="Chest">
                     <img className="card-img-top" src="./images/chest.jpg" alt="Card image cap" />
                     <div className="card-body">
                         <h5 className="card-title text-center">Chest</h5>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card btn" data-toggle="modal"  data-target="#workoutModal" value="Legs">
                     <img className="card-img-top" src="./images/legs.jpg" alt="Card image cap" />
                     <div className="card-body ">
                         <h5 className="card-title text-center">Legs</h5>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card btn" data-toggle="modal"  data-target="#workoutModal" value="Core">
                     <img className="card-img-top" src="./images/core.jpg" alt="Card image cap" />
                     <div className="card-body">
                         <h5 className="card-title text-center">Core</h5>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card btn" data-toggle="modal"  data-target="#workoutModal" value="Back">
                     <img className="card-img-top" src="./images/back.jpg" alt="Card image cap" />
                     <div className="card-body">
                         <h5 className="card-title text-center">Back</h5>
                     </div>
                 </div>
-                <button type="button" className=" btn btn-primary" data-toggle="modal"  data-target="#workoutModal">Modal test</button>
+                <div className="card btn" data-toggle="modal"  data-target="#workoutModal" value="cardio">
+                    <img className="card-img-top" src="./images/heartrate.jpg" alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Cardio</h5>
+                    </div>
+                </div>
             </div>
         );
     };
